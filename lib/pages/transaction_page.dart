689a88d44166d83cc4c546c7cacd2ca5a404a551
AnonymoxB <Anonymoxb12@gmail.com>
2023-09-18 22:21:09 +0700

    y

--- lib/pages/transaction_page.dart
+++ lib/pages/transaction_page.dart
@@ -11,6 +11,9 @@ class TransactionPage extends StatefulWidget {
 
 class _TransactionPageState extends State<TransactionPage> {
   bool isExpens = true;
+  List<String> list = ['Makan dan Jajan', 'Transportasi', 'Nonton Film'];
+  late String dropDownValue = list.first;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +23,7 @@ class _TransactionPageState extends State<TransactionPage> {
       body: SingleChildScrollView(
         child: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
                 children: [
@@ -40,13 +44,47 @@ class _TransactionPageState extends State<TransactionPage> {
                   )
                 ],
               ),
+              SizedBox(
+                height: 10,
+              ),
               Padding(
-                padding: const EdgeInsets.all(16),
+                padding: const EdgeInsets.symmetric(horizontal: 16),
                 child: TextFormField(
                   keyboardType: TextInputType.number,
                   decoration: InputDecoration(
                       border: UnderlineInputBorder(), labelText: "Jumlah"),
                 ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Text(
+                  'Kategori',
+                  style: GoogleFonts.montserrat(fontSize: 16),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: DropdownButton<String>(
+                    value: dropDownValue,
+                    isExpanded: true,
+                    icon: Icon(Icons.arrow_downward),
+                    items: list.map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (String? value) {}),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: "Pilih Tanggal"),
+                onTap: () {},
               )
             ],
           ),
